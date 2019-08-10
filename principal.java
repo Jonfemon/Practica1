7fba0db0a2bbe492b012114c786356a6efd1462d
Jonfemon <diegojonfebetan@hotmail.com>
2019-08-09 18:28:32 -0600

    primer comit

---
+++ principal.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Principal {
+
+    public static void main (String[] args) {
+	system.out.println ("hola Mundo");
+	int a;
+	Random random = new Random;
+
+	a=random;
+		System.out.println(a);
+
+ 	}
+}
